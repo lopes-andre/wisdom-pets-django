6191ad2a5b403e7088c4b76df4e1e412780e9787
André Lopes <lopes.andre@outlook.com>
2021-09-22 12:10:39 -0300

    Update de template's DIRS

--- wisdompets/wisdompets/settings.py
+++ wisdompets/wisdompets/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'wisdompets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/adoptions/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
