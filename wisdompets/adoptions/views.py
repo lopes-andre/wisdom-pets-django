b93e0910e0cfa341907e65e257dc2bf39d3624e2
André Lopes <lopes.andre@outlook.com>
2021-09-22 11:45:06 -0300

    Create the pet_detail view

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<p>Test home view</p>')
+
+def pet_detail(request, pet_id):
+    return HttpResponse('<h1>Test the pet detail view</h1>')
